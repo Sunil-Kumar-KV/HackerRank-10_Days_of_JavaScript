eb5c29b40a041a304d749927cf5c544fb9e35198
Sunil Kumar K V <111721933+SunilKumarKV@users.noreply.github.com>
2023-07-24 19:59:45 +0530

    Update Day 3: Try, Catch, and Finally.js

--- Day 3: Try, Catch, and Finally.js
+++ Day 3: Try, Catch, and Finally.js
@@ -21,3 +21,19 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        s = s.split('').reverse().join('');
+    }
+    catch(e) {
+        console.log(e.message);
+    }
+    finally {
+        console.log(s);
+    }
+}
