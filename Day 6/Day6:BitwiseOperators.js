a6976d5ebf26bb384cfffa1e1c1b6d78335612c5
Sunil Kumar K V <sunilkvb44@gmail.com>
2025-02-14 18:37:11 +0530

    Update Day6:BitwiseOperators.js

--- Day 6/Day6:BitwiseOperators.js
+++ Day 6/Day6:BitwiseOperators.js
@@ -22,6 +22,19 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+function getMaxLessThanK(n, k) {
+    let max = 0;
+
+    for (let a = 1; a <= n; a++) {
+        for (let b = a + 1; b <= n; b++) {
+            let bw = (a & b);
+
+            (bw < k && bw > max) && (max = bw);
+        }
+    }
+ 
+    return max;
+}
 
 function main() {
     const q = +(readLine());
